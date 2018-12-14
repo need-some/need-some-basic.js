9e889d646a3785c68c3bc710e1d2c13396a850e7
Kristofer Treutwein <kristofer@treutwein.org>
2018-12-14 08:14:31 +0100

    set version to 0.0.16

--- src/module/converter/tokenformatter.ts
+++ src/module/converter/tokenformatter.ts
@@ -49,9 +49,73 @@ export function toUpperCase(locale: string): StringTokenFormat {
 }
 
 /**
+ * Creates a formatter that changes all characters to lower case.
+ * @param locale The locale to use for lower case. TODO: Currently this locale is ignored.
+ */
+export function toLowerCase(locale: string): StringTokenFormat {
+	return input => ('' + input).toLowerCase(); // TODO: use locale
+}
+
+/**
  * Creates a formatter that changes the first character to upper case.
  * @param locale The locale to use for upper case. TODO: Currently this locale is ignored.
  */
 export function firstToUpper(locale: string): StringTokenFormat {
-	return input => ('' + input).toUpperCase().substr(0, 1) + ('' + input).substr(1); // TODO: use locale
+	return input => (('' + input).substr(0, 1)).toUpperCase().substr(0, 1) + ('' + input).substr(1); // TODO: use locale
+}
+
+/**
+ * Creates a formatter that changes the first character to lower case.
+ * @param locale The locale to use for lower case. TODO: Currently this locale is ignored.
+ */
+export function firstToLower(locale: string): StringTokenFormat {
+	return input => (('' + input).substr(0, 1)).toLowerCase().substr(0, 1) + ('' + input).substr(1); // TODO: use locale
+}
+
+/**
+ * Creates a formatter that changes the case of every character.
+ * @param locale The locale to use for upper/lower case. TODO: Currently this locale is ignored.
+ */
+export function toInverseCase(locale: string): StringTokenFormat {
+	const lower = toLowerCase(locale);
+	const upper = toUpperCase(locale);
+	return input => {
+		const allLower = lower(input).split('');
+		const allUpper = upper(input).split('');
+		return (input + '').split('').map((c, i) => allLower[i] === c ? allUpper[i] : allLower[i]).join('');
+	};
+}
+
+/**
+ * Creates a formatter that changes the case of the first character.
+ * @param locale The locale to use for upper/lower case. TODO: Currently this locale is ignored.
+ */
+export function firstToInverse(locale: string): StringTokenFormat {
+	const inverse = toInverseCase(locale);
+	return input => inverse(('' + input).substr(0, 1)) + ('' + input).substr(1); // TODO: use locale
+}
+
+/**
+ * Creates a formatter that applies all functions to the result of the former.
+ * @param first the initial formatter
+ * @param formatters the following formatters
+ */
+export function concat(first: NumberTokenFormat, ...formatters: StringTokenFormat[]): NumberTokenFormat;
+
+/**
+ * Creates a formatter that applies all functions to the result of the former.
+ * @param first the initial formatter
+ * @param formatters the following formatters
+ */
+export function concat(first: StringTokenFormat, ...formatters: StringTokenFormat[]): StringTokenFormat;
+
+/**
+ * Creates a formatter that applies all functions to the result of the former.
+ * @param first the initial formatter
+ * @param formatters the following formatters
+ */
+export function concat(first: StringTokenFormat|NumberTokenFormat, ...formatters: StringTokenFormat[]): StringTokenFormat {
+	return input => {
+		return formatters.reduce((value, formatter) => formatter(value), (<StringTokenFormat>first)(input));
+	};
 }
--- src/test/converter/tokenformatter.spec.ts
+++ src/test/converter/tokenformatter.spec.ts
@@ -1,5 +1,5 @@
 import { pit } from '@need-some/test';
-import { padNumberLeft, padStringLeft, padStringRight, toUpperCase, firstToUpper } from '../../module/converter/tokenformatter';
+import { padNumberLeft, padStringLeft, padStringRight, toUpperCase, firstToUpper, toInverseCase, firstToInverse, concat, StringTokenFormat } from '../../module/converter/tokenformatter';
 
 describe('TokenFormatter', () => {
 	const params = [
@@ -10,7 +10,7 @@ describe('TokenFormatter', () => {
 			padLen: 2,
 			padString: ' ',
 			locale: 'de',
-			expected: { padNumberLeft: '', padStringLeft: '  ', padStringRight: '  ', toUpperCase: '', firstToUpper: '' }
+			expected: { padNumberLeft: '', padStringLeft: '  ', padStringRight: '  ', toUpperCase: '', firstToUpper: '', inverse: '', concat: 'AaAa', concatNumber: '' }
 		},
 		{
 			title: 'zero',
@@ -19,7 +19,7 @@ describe('TokenFormatter', () => {
 			padLen: 3,
 			padString: ' ',
 			locale: 'de',
-			expected: { padNumberLeft: '000', padStringLeft: '  0', padStringRight: '0  ', toUpperCase: '0', firstToUpper: '0' }
+			expected: { padNumberLeft: '000', padStringLeft: '  0', padStringRight: '0  ', toUpperCase: '0', firstToUpper: '0', inverse: '0', concat: 'Aa0Aa', concatNumber: '#####0' }
 		},
 		{
 			title: 'a string',
@@ -28,7 +28,16 @@ describe('TokenFormatter', () => {
 			padLen: 5,
 			padString: '#',
 			locale: 'en',
-			expected: { padNumberLeft: '', padStringLeft: '##abc', padStringRight: 'abc##', toUpperCase: 'ABC', firstToUpper: 'Abc' }
+			expected: { padNumberLeft: '', padStringLeft: '##abc', padStringRight: 'abc##', toUpperCase: 'ABC', firstToUpper: 'Abc', inverse: 'ABC', concat: 'AaABCAa', concatNumber: '' }
+		},
+		{
+			title: 'a mixed case string',
+			input: 'abcDef',
+			inputNumeric: false,
+			padLen: 7,
+			padString: '#',
+			locale: 'en',
+			expected: { padNumberLeft: '', padStringLeft: '#abcDef', padStringRight: 'abcDef#', toUpperCase: 'ABCDEF', firstToUpper: 'AbcDef', inverse: 'ABCdEF', concat: 'AaABCdEFAa', concatNumber: '' }
 		},
 		{
 			title: 'a negative number',
@@ -37,7 +46,7 @@ describe('TokenFormatter', () => {
 			padLen: 4,
 			padString: ' ',
 			locale: 'en',
-			expected: { padNumberLeft: '-023', padStringLeft: ' -23', padStringRight: '-23 ', toUpperCase: '-23', firstToUpper: '-23' }
+			expected: { padNumberLeft: '-023', padStringLeft: ' -23', padStringRight: '-23 ', toUpperCase: '-23', firstToUpper: '-23', inverse: '-23', concat: 'Aa-23Aa', concatNumber: '####23' }
 		},
 		{
 			title: 'a long number',
@@ -46,7 +55,7 @@ describe('TokenFormatter', () => {
 			padLen: 2,
 			padString: '#',
 			locale: 'en',
-			expected: { padNumberLeft: '1999', padStringLeft: '1999', padStringRight: '1999', toUpperCase: '1999', firstToUpper: '1999' }
+			expected: { padNumberLeft: '1999', padStringLeft: '1999', padStringRight: '1999', toUpperCase: '1999', firstToUpper: '1999', inverse: '1999', concat: 'Aa1999Aa', concatNumber: '#-1999' }
 		}
 	];
 	pit('should format ${title} with padNumberLeft', params.filter(p => p.inputNumeric), param => {
@@ -73,5 +82,22 @@ describe('TokenFormatter', () => {
 		const sut = firstToUpper(param.locale);
 		const result = sut(param.input);
 		expect(result).toBe(param.expected.firstToUpper);
+	});
+	pit('should format ${title} with toInverseCase', params, param => {
+		const sut = toInverseCase(param.locale);
+		const result = sut(param.input);
+		expect(result).toBe(param.expected.inverse);
+	});
+	pit('should format ${title} with concat', params, param => {
+		const wrap = (s: string) => ("a" + s + "a");
+		const sut = concat(wrap, firstToUpper(param.locale), toInverseCase(param.locale), wrap, firstToInverse(param.locale));
+		const result = sut(param.input);
+		expect(result).toBe(param.expected.concat);
+	});
+	pit('should format ${title} with number concat', params.filter(p => p.inputNumeric), param => {
+		const wrap = (n: number) => ("" + (-n));
+		const sut = concat(wrap, padStringLeft('#', 6));
+		const result = sut(+param.input);
+		expect(result).toBe(param.expected.concatNumber);
 	});
 });
