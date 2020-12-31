79bf43be55705558569dba026b770338f1ea2c51
Kristofer Treutwein <kristofer@treutwein.org>
2020-12-31 21:35:57 +0100

    set version to 0.1.2

--- karma.conf.js
+++ karma.conf.js
@@ -1,31 +1,41 @@
 module.exports = function(config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', 'karma-typescript'],
-    plugins: [
-      require('karma-typescript'),
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
-    },
-    files: [
-      'src/**/*.ts' // *.tsx for React Jsx
-    ],
-    preprocessors: {
-      'src/**/*.ts': 'karma-typescript' // *.tsx for React Jsx
-    },
-    reporters: ['progress', 'karma-typescript', 'kjhtml'],
-    browsers: ['Chrome'],
-    singleRun: false,
-    autoWatch: true
-  });
+	config.set({
+		basePath: '',
+		frameworks: ['jasmine', 'karma-typescript'],
+		plugins: [
+			require('karma-typescript'),
+			require('karma-jasmine'),
+			require('karma-chrome-launcher'),
+			require('karma-jasmine-html-reporter'),
+			require('karma-coverage-istanbul-reporter')
+		],
+		karmaTypescriptConfig: {
+			tsconfig: './tsconfig.karma.json'
+		},
+		client: {
+			clearContext: false, // leave Jasmine Spec Runner output visible in browser
+			captureConsole: true
+		},
+		browserConsoleLogOptions: {
+			level: 'log',
+			format: '%b %T: %m',
+			terminal: true
+		},
+		logLevel: config.LOG_LOG,
+		coverageIstanbulReporter: {
+			dir: require('path').join(__dirname, 'coverage'),
+			reports: ['html', 'lcovonly'],
+			fixWebpackSourcePaths: true
+		},
+		files: [
+			'src/**/*.ts' // *.tsx for React Jsx
+		],
+		preprocessors: {
+			'src/**/*.ts': 'karma-typescript' // *.tsx for React Jsx
+		},
+		reporters: ['progress', 'karma-typescript', 'kjhtml'],
+		browsers: ['Chrome'],
+		singleRun: false,
+		autoWatch: true
+	});
 };
